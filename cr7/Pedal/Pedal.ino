e4ccea0dc160c95a14e449f92d99b1c21e09db2e
Guilherme Souza <98468033+camposouza@users.noreply.github.com>
2022-11-21 20:07:55 -0300

    Update Pedal.ino
    Correção no código

--- cr7/Pedal/Pedal.ino
+++ cr7/Pedal/Pedal.ino
@@ -1,12 +1,14 @@
-#define POT_ACELERADOR A8
-
 #define pedalGND A8
 #define pedal A9
 #define pedalVCC A10
 
 void setup() {
   Serial.begin(9600);
-  pinMode(pedal,INPUT);
+  pinMode(pedalGND, OUTPUT);
+  pinMode(pedal, INPUT);
+  pinMode(pedalVCC, OUTPUT);
+  analogWrite(pedalGND, 0); // Pino 8 -> GND 
+  analogWrite(pedalVCC, 1023); // Pino 10 -> VCC
 }
 
 void loop() {
