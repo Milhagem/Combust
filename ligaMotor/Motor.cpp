10decb95e2f16d5f2664ad7efaea89d9bcbdceda
camposouza <guilhermesouza0911@outlook.com>
2022-12-02 17:06:33 -0300

    Criando Motor.h e Motor.cpp

---
+++ ligaMotor/Motor.cpp
@@ -0,0 +1,49 @@
+#include "Motor.h"
+
+float Motor::analiseTensao(){
+  float valorInicial = analogRead(A1);
+  float tensao = (valorInicial*5.0) / 1024; 
+  return tensao;
+}
+
+void Motor::ligaMotor(){
+  digitalWrite(pLigaMotor, HIGH);
+  Serial.println("Ligando Motor");
+    
+  // Verifica a tensao por 1s
+  for (int i = 0; i < 10; i++) {    
+    delay(100);
+    float tensao = analiseTensao();
+    if(tensao < (tensaoLigado+1) && tensao > (tensaoLigado-1)) { i++; } 
+    else { i = 0; }
+  }
+
+  digitalWrite(pLigaMotor,LOW);  // liga motor de arranque por 800ms ??????
+  estadoMotor = LIGADO;
+  digitalWrite(9, HIGH);
+
+  Serial.println("Motor ligado");
+  printVelocidade();
+}
+
+
+
+void Motor::desligaMotor(){
+  int pos = 0;
+  servo.write(pos);
+
+  digitalWrite(pDesligaMotor,HIGH);
+  digitalWrite(9,LOW);
+  delay(800);
+
+  digitalWrite(pDesligaMotor,LOW);
+  estadoMotor = DESLIGADO;
+
+  Serial.println("Motor desligado");
+  printVelocidade();
+}
+
+void Motor::printVelocidade(){
+  Serial.print("Velocidade: "); 
+  Serial.println(analogRead(A0)); 
+}
---
+++ ligaMotor/Motor.h
@@ -0,0 +1,30 @@
+#ifndef Motor_h
+#define Motor_h
+
+#include "Arduino.h"
+#include "Servo.h"
+
+#define LIGADO 1
+#define DESLIGADO 0
+
+class Motor{
+  private:
+    Servo servo;
+    int pLigaMotor;
+    int pDesligaMotor;
+    bool estadoMotor;
+    float tensaoLigado;
+
+  public:
+    float analiseTensao();
+    void ligaMotor();
+    /**
+    * @brief Apos velocidade max ser alcançada, desliga motor e volta servo pra posicao inicial
+    *
+    */
+    void desligaMotor();
+    void printVelocidade();
+
+};
+
+#endif
