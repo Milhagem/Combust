8eb5f5414c89fcd68a3c54e6ee325d840c26302b
camposouza <guilhermesouza0911@outlook.com>
2022-12-02 11:46:32 -0300

    .vscode para .gitignore

--- cr7/pedal/pedal.ino
+++ cr7/pedal/pedal.ino
@@ -1,14 +1,14 @@
 #define pedalGND A8
 #define pedal A9
-#define pedalVCC A10
+#define pedalVcc A10
 
 void setup() {
   Serial.begin(9600);
   pinMode(pedalGND, OUTPUT);
   pinMode(pedal, INPUT);
-  pinMode(pedalVCC, OUTPUT);
+  pinMode(pedalVcc, OUTPUT);
   analogWrite(pedalGND, 0); // Pino 8 -> GND 
-  analogWrite(pedalVCC, 1023); // Pino 10 -> VCC
+  analogWrite(pedalVcc, 1023); // Pino 10 -> VCC
 }
 
 void loop() {
