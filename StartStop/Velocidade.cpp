015eeb5ff0cb4763504d41583cbc3d97a88b7c20
camposouza <guilhermesouza0911@outlook.com>
2024-06-28 15:21:22 -0300

    novo codigo calcula velocidade

--- StartStop/Velocidade.cpp
+++ StartStop/Velocidade.cpp
@@ -1,26 +1,60 @@
 #include "Velocidade.h"
 
-int velocidadeAtual;          // km/h
-unsigned long lastmillis;     // ms
+const unsigned long maximoSample = MS_to_S * circunfRoda/(pulsosPorVolta) * MPS_to_KMPH; // Isso não faz o menor sentido!!!!!!!!
+
 volatile int picoLeituraHall;
 
-int atualizaVelocidadeAtual(int velocidadeAtual) {
-  if((millis() - lastmillis) > taxaAtualizacaoVel){ 
-    
+unsigned long lastPulseTime; // ms
+unsigned long currentMillis; // ms
+unsigned long timecontador;  // ms
+unsigned long pulseTime;     // ms
+float speed;            // km/h
+float last_speed;
+
+unsigned long pulseTimes[sampleSize];
+int pulseIndex;
+
+float calculaVelocidade(float speed) {
+  currentMillis = millis();
+  pulseTime = currentMillis - lastPulseTime;
+
+  if(pulseTime >= taxaAtualizacaoVel) {
+    currentMillis = millis();
+    lastPulseTime = currentMillis;
     detachInterrupt(digitalPinToInterrupt(pinSensorHall));
-
-    float distanciaPercorrida = picoLeituraHall/quantImas * diametroRodaURban; // m
-    float velocidadeRaw = distanciaPercorrida/taxaAtualizacaoVel;             // m/ms
-    float velocidadeNew = velocidadeRaw * MS_to_S * MPS_to_KMPH;                
-
-    lastmillis = millis();
-    picoLeituraHall = 0;
-
-    attachInterrupt(digitalPinToInterrupt(pinSensorHall), variador, RISING);
-
-    return velocidadeNew;
+    unsigned long averagePulseTime = 0;  // ms
+    for (int i = 0; i < sampleSize; i++) {
+      averagePulseTime += pulseTimes[i];
+    }
+    averagePulseTime /= sampleSize;
+    speed = circunfRoda/(pulsosPorVolta*averagePulseTime) * MS_to_S * MPS_to_KMPH;
+    attachInterrupt(digitalPinToInterrupt(pinSensorHall), calc, RISING);
   }
-  return velocidadeAtual;
 }
 
-void variador() { picoLeituraHall++; }
+float filtroVelocidade(float velocidadeAtual, float velocidadeNew) {
+  const int limiteSuperiorVel = 1.9;
+  const int limiteInferiorVel = 0.4;
+
+  if(velocidadeNew >= limiteSuperiorVel*speed && velocidadeAtual != 0){
+    return velocidadeAtual;
+  } else if(velocidadeNew <= limiteInferiorVel*speed && velocidadeAtual != 0){
+    return velocidadeAtual;
+  } else {
+    return velocidadeNew;
+  }
+}
+
+void calc() {
+  currentMillis = millis();
+  pulseTime = currentMillis - lastPulseTime;
+  timecontador = millis();
+  lastPulseTime = currentMillis;
+  pulseTimes[pulseIndex] = pulseTime;
+  pulseIndex = (pulseIndex + 1) % sampleSize; 
+  lastPulseTime = pulseTime;
+}
+
+int calculaVelocidade_Pedal(int pinPedal) {
+  return analogRead(pinPedal);
+}
--- StartStop/Velocidade.h
+++ StartStop/Velocidade.h
@@ -8,26 +8,48 @@
 #define pinPedalVCC        A9  // Pino p/ simulacao de velocidade em testes em bancada
 #define pinVelPedal        A10 // Pino p/ simulacao de velocidade em testes em bancada
 
-#define velZERO             150  // Valores para teste
-#define velMin              400  // Valores para teste
-#define velMax              700  // Valores para teste
-#define taxaAtualizacaoVel 1000  // ms
-#define diametroRodaURban  0.015 // m
-#define quantImas          5
+#define velZERO            150  // Valores para teste
+#define velMin             400  // Valores para teste
+#define velMax             700  // Valores para teste
+#define taxaAtualizacaoVel 1000 // ms
+#define circunfRoda        0.47 // m
+#define pulsosPorVolta     5
+#define sampleSize         3    // Numero de amostras para calcular velocidade
 
-#define MPS_to_KMPH   3.6 // metros por segundo p/ quilometro por hora
-#define MS_to_S     000.1 // milisegundos p/ segundos
+#define MPS_to_KMPH 3.6  // metros por segundo p/ quilometro por hora
+#define MS_to_S     1000 // milisegundos p/ segundos
 
-void variador();
+/** 
+ * @brief Armazena o valor de periodo da onda no vetor usado para o calculo de velocidade
+ * 
+ */
+void calc();
+
 /**
- * @brief Atualiza a velocidade com base nos pulsos do Sensor Hall
+ * @brief Atualiza a velocidade com base nos pulsos do Sensor Hall 
  * 
- * @details Usa a interrupcao do Arduino para computar a quantidade de pulsos em um intervalo de tempo. Retorna a velocidade atualiza OU a velocidade nao atualizada (passada como parametro).
+ * @details DEFINIR
  * 
- * @param velocidadeAtual velocidade antes da chamada da funcao. Sera retornada caso a velocidade nao seja atualizada.
+ * @param velocidadeAtual velocidade antes da chamada da funcao.
  * 
  * @return velocidade (m/s)
  */
-int atualizaVelocidadeAtual(int velocidadeAtual);
+float calculaVelocidade(float speed);
+
+/**
+ * @brief Filtra variacoes absuradas de velocidade
+ * 
+ * @param velocidadeRaw 
+ * @return velocidade (km/h) 
+ */
+float filtroVelocidade(float velocidade, float velocidadeNew);
+
+/**
+ * @brief retorna um valor de velocidade com base na leitura de um pino analogico para testes em bancada
+ * 
+ * @param pinPedal 
+ * @return int entre 0 e 1023
+ */
+int calculaVelocidade_Pedal(int pinPedal);
 
 #endif
