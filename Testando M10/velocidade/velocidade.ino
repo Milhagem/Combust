183546dda7e0db4a291c4a221cdd31680c025034
camposouza <guilhermesouza0911@outlook.com>
2024-06-29 00:19:35 -0300

    mudança nas variaveis de calculo de tempo

--- Testando M10/velocidade/velocidade.ino
+++ Testando M10/velocidade/velocidade.ino
@@ -1,26 +1,24 @@
 #include "Arduino.h"
 
-#define pinSensorHall DD2 
+#define pinSensorHall DD2
 
 #define pulsosPorVolta     5
 #define sampleSize         3
 #define velocidadeMaxima   35   // km/h
-#define taxaAtualizacaoVel 1000 // ms
+#define taxaAtualizacaoVel 100 // ms
 #define circunfRoda        0.47  // m
 
 #define MPS_to_KMPH 3.6  // metros por segundo p/ quilometros por hora
 #define MS_to_S     1000 // milisegundos p/ segundos
 
-const unsigned long maximoSample = MS_to_S * circunfRoda/(pulsosPorVolta) * MPS_to_KMPH; // ?
-
-unsigned long lastPulseTime; // ms
-unsigned long currentMillis; // ms
-unsigned long timeContador;  // ms
-unsigned long pulseTime;     // ms
-float speed;                 // km/h
+unsigned long pulseInterval;
+unsigned long lastPulseInterval;
+unsigned long timerCalcVel;     // ms
+unsigned long lastTimerCalcVel; // ms
+float velocidade;                 // km/h
+float lastVelocidade;
 
 unsigned long pulseTimes[sampleSize];
-int lastSpeed;
 int pulseIndex;
 
 /**
@@ -30,56 +28,49 @@ int pulseIndex;
 void calc();
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   pinMode(pinSensorHall, INPUT);
 
   attachInterrupt(digitalPinToInterrupt(pinSensorHall), calc, RISING);
   
+  pulseInterval = 0;
+  lastPulseInterval = 0;
+  timerCalcVel = 0;
+  lastTimerCalcVel = 0;
+  velocidade = 0.0;
+  lastVelocidade = 0.0;
   for (int i = 0; i < sampleSize; i++) { pulseTimes[i] = 0; }
-
-  lastPulseTime = 0;
-  currentMillis = 0;
-  timeContador = 0;
-  pulseTime = 0;
-  speed = 0.0;
-  lastSpeed = 0;
   pulseIndex = 0;
 }
 
 void loop() {
-  currentMillis = millis();
-  pulseTime = currentMillis - lastPulseTime;
-
-  if(pulseTime >= taxaAtualizacaoVel) {
-    currentMillis = millis();
-    lastPulseTime = currentMillis;
+  timerCalcVel = millis() - lastTimerCalcVel;
+  if(timerCalcVel >= taxaAtualizacaoVel) {
+    lastTimerCalcVel = millis();
     detachInterrupt(digitalPinToInterrupt(pinSensorHall));
     unsigned long averagePulseTime = 0;  // ms
     for (int i = 0; i < sampleSize; i++) {
       averagePulseTime += pulseTimes[i];
     }
     averagePulseTime /= sampleSize;
-    if(speed >= 1.9*lastSpeed && speed != 0){
-    speed = lastSpeed;
-    } else if(speed <= 0.4*lastSpeed && speed != 0){
-    speed = lastSpeed;
+    if(velocidade >= 1.4*lastVelocidade && velocidade != 0){
+    velocidade = lastVelocidade;
+    } else if(velocidade <= 0.4*lastVelocidade && velocidade != 0){
+    velocidade = lastVelocidade;
     } else {
-      speed = circunfRoda/(pulsosPorVolta*averagePulseTime) * MS_to_S * MPS_to_KMPH;
+      velocidade = circunfRoda/(pulsosPorVolta*averagePulseTime) * MS_to_S * MPS_to_KMPH;
     }  
-    lastSpeed = speed;
+    lastVelocidade = velocidade;
     attachInterrupt(digitalPinToInterrupt(pinSensorHall), calc, RISING);
-    Serial.print("Speed:");
-    Serial.println(speed);
+    Serial.print("velocidade:");
+    Serial.println(velocidade);
   }
 }
 
 void calc() {
-  currentMillis = millis();
-  pulseTime = currentMillis - lastPulseTime;
-  timeContador = millis();
-  lastPulseTime = currentMillis;
-  pulseTimes[pulseIndex] = pulseTime;
+  pulseInterval = millis() - lastPulseInterval;
+  lastPulseInterval = millis();
+  pulseTimes[pulseIndex] = pulseInterval;
   pulseIndex = (pulseIndex + 1) % sampleSize; 
-  lastPulseTime = pulseTime;
 }
