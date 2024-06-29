ffe057d5688b21dd05d8c1dfcbe1434e5d6526a5
camposouza <guilhermesouza0911@outlook.com>
2024-06-29 00:20:02 -0300

    Calculo de velocidade por ponteiro

--- Testando M10/velocidade_modularizado/velocidade_modularizado.ino
+++ Testando M10/velocidade_modularizado/velocidade_modularizado.ino
@@ -13,12 +13,11 @@
 #define MPS_to_KMPH 3.6  // metros por segundo p/ quilometro por hora
 #define MS_to_S     1000 // milisegundos p/ segundos
 
-unsigned long lastPulseTime; // ms
-unsigned long currentMillis; // ms
-unsigned long timeContador;  // ms
-unsigned long pulseTime;     // ms
-float speed;                 // km/h
-float lastSpeed;             // km/h         
+unsigned long timerCalcVel;         // ms
+unsigned long lastTimerCalcVel;     // ms
+unsigned long pulseInterval;     // ms
+unsigned long lastPulseInterval; // ms
+float velocidade;                // km/h     
 
 unsigned long pulseTimes[sampleSize];
 int pulseIndex;
@@ -38,66 +37,48 @@ void calc();
  * 
  * @return velocidade (m/s)
  */
-void calculaVelocidade(float speed);
+void calculaVelocidade(float &veloc) {
+  timerCalcVel = millis() - lastTimerCalcVel;
+
+  if(timerCalcVel >= taxaAtualizacaoVel) {
+    timerCalcVel = millis();
+
+    detachInterrupt(digitalPinToInterrupt(pinSensorHall));
+//    unsigned long averagePulseInterval = 0;  // ms
+//    for (int i = 0; i < sampleSize; i++) {
+//      averagePulseInterval += pulseTimes[i];
+//    }
+//    averagePulseInterval /= sampleSize;
+    veloc = circunfRoda/(pulsosPorVolta*pulseInterval) * MS_to_S * MPS_to_KMPH;
+    attachInterrupt(digitalPinToInterrupt(pinSensorHall), calc, RISING);
+    Serial.print("veloc:");
+    Serial.println(veloc);
+  }
+}
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   pinMode(pinSensorHall, INPUT);
 
   attachInterrupt(digitalPinToInterrupt(pinSensorHall), calc, RISING);
-
-  lastPulseTime = 0;
-  currentMillis = 0;
-  timeContador = 0;
-  pulseTime = 0;
-  speed = 0.0;
-  lastSpeed = 0;
+  
+  timerCalcVel = 0;
+  lastTimerCalcVel = 0;
+  pulseInterval = 0;
+  lastPulseInterval = 0;
+  velocidade = 0.0;
   for (int i = 0; i < sampleSize; i++) { pulseTimes[i] = 0; }
   pulseIndex = 0;
 }
 
 void loop() {
-    calculaVelocidade(speed);
-
-    // Serial.print("VelocidadeAtual:");
-    // Serial.println(velocidadeAtual);
+    calculaVelocidade(velocidade);
 }
 
 void calc() {
-  currentMillis = millis();
-  pulseTime = currentMillis - lastPulseTime;
-  timeContador = millis();
-  lastPulseTime = currentMillis;
-  pulseTimes[pulseIndex] = pulseTime;
-  pulseIndex = (pulseIndex + 1) % sampleSize; 
-  lastPulseTime = pulseTime; 
-}
-
-void calculaVelocidade(float speed) {
-  currentMillis = millis();
-  pulseTime = currentMillis - lastPulseTime;
-
-  if(pulseTime >= taxaAtualizacaoVel) {
-    currentMillis = millis();
-    lastPulseTime = currentMillis;
-
-    detachInterrupt(digitalPinToInterrupt(pinSensorHall));
-    unsigned long averagePulseTime = 0;  // ms
-    for (int i = 0; i < sampleSize; i++) {
-      averagePulseTime += pulseTimes[i];
-    }
-    averagePulseTime /= sampleSize;
-    if(speed >= 1.9*lastSpeed && speed != 0){
-    speed = lastSpeed;
-    } else if(speed <= 0.4*lastSpeed && speed != 0){
-    speed = lastSpeed;
-    } else {
-    speed = circunfRoda/(pulsosPorVolta*averagePulseTime) * MS_to_S * MPS_to_KMPH;
-    }
-    lastSpeed = speed;
-    attachInterrupt(digitalPinToInterrupt(pinSensorHall), calc, RISING);
-    Serial.print("Speed:");
-    Serial.println(speed);
-  }
+  pulseInterval = millis() - lastPulseInterval;
+  lastPulseInterval = millis();
+//  pulseTimes[pulseIndex] = timerCalcVel;
+//  pulseIndex = (pulseIndex + 1) % sampleSize; 
 }
