5047cda8fe84190dd0d1ac539a3188928c7516e9
Pedroedroedro <pedroandcastro@gmail.com>
2025-02-11 16:33:45 -0300

    atualizacao pedro

---
+++ StartStop V1.2/IncrementaVelocidadepedro.cpp
@@ -0,0 +1,39 @@
+#include "IncrementaVelocidade.h"
+
+//int posServo;                       // graus
+//unsigned long timerIncrementoServo = 100; // ms
+
+void giraServoMotor_aceleracao(Motor &motor, int &posServo) {
+        if (/*motor.analisaTensao() < TensaoMotorAcelerando && */posServo < posInicialServo) {
+            posServo = posInicialServo;
+            motor.servoWrite(posServo);
+            //timerIncrementoServo = millis();
+        } else if(posServo <= 25) {
+            posServo += increvementoServo;
+            motor.servoWrite(posServo);
+            //timerIncrementoServo = millis();
+        }
+        
+        //return posServo;
+}
+
+void giraServoMotor_estabilizacao(Motor &motor, int &posServo) {
+        if (/*motor.analisaTensao() < TensaoMotorAcelerando && */posServo < posInicialServo) {
+            posServo = posInicialServo;
+            motor.servoWrite(posServo);
+            //timerIncrementoServo = millis();
+        } else if(posServo <= 25) {
+            posServo--; //criei uma funcao pra decrementar pos do servo
+            motor.servoWrite(posServo);
+            //timerIncrementoServo = millis();
+        }
+        
+        //return posServo;
+}
+
+void giraServoMotor_desaceleracao(Motor &motor) {
+        motor.servoWrite(0);
+        //return posServo;
+}
+
+//é preciso elaborar mais a função de incremento do servo e decremento so servo
