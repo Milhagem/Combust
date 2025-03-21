ee575f6134bd4316c44afde5e339792c2f00c656
Arthur <arthurmesquitasan@gmail.com>
2025-03-21 15:59:16 -0300

    Novos_Testes

---
+++ V1.3.2/TesteModular/Display.cpp
@@ -0,0 +1,99 @@
+#include "Display.hpp"
+
+//volatile unsigned long timeOld = 0;
+
+Display::Display (){
+  this->lcd.init();
+  this->lcd.backlight();
+  this->lcd.setCursor(0,0);
+  this->lcd.print("Iniciando M10");
+  delay(500);
+  
+}
+
+void Display::mostraTensaoEVel(float velocidade){
+    if ( (millis() - timeOld) >= timeInterval){
+
+    //this->lcd.setCursor(0,0);
+    //this->lcd.print("DDP:    ");
+    //this->lcd.setCursor(5,0);
+    //this->lcd.print(motor.analisaTensao());
+    // Exibir velocidade
+    this->lcd.setCursor(0,1);
+    this->lcd.print("Vel:            ");
+    this->lcd.setCursor(6,1);
+    this->lcd.print(velocidade);
+
+    timeOld = millis();
+  }
+}
+
+void Display::atualizaDisplay( float velocidade, int FSMState) {
+  mostraTensaoEVel(velocidade);
+
+  String FSMState_str;
+
+  switch (FSMState)
+  {
+    case 1:
+      FSMState_str = "SS_off";
+      break;
+
+    case 2:
+      FSMState_str = "SS__on";
+      break;
+      
+      case 3:
+      FSMState_str = "deslgM";
+      break;
+      
+      case 4:
+      FSMState_str = "ligaM ";
+      break;
+      
+      case 5:
+      FSMState_str = "estabA";
+      break;
+      
+      case 6:
+      FSMState_str = "estabV";
+      break;
+
+      case 7:
+      FSMState_str = "manipB";
+      break;
+
+      case 8:
+      FSMState_str = "iniciou";
+      break;
+
+      case 9:
+      FSMState_str = "parou";
+      break;
+      
+      case 10:
+      FSMState_str = "freou";
+      break;
+
+      case 11:
+      FSMState_str = "notLig";
+      break;
+
+      case 12:
+      FSMState_str = "notDsg";
+      break;
+      
+      case 13:
+      FSMState_str = "deslgSS";
+      break;
+      
+      default:
+      break;
+  }
+
+  this->lcd.setCursor(10, 0);
+  this->lcd.print(FSMState_str);
+
+  this->lcd.setCursor(15,1);
+  //this->lcd.print(motor.checaEstadoMotor());
+}
---
+++ V1.3.2/TesteModular/StartStop.hpp
@@ -0,0 +1,85 @@
+#ifndef STARTSTOP_H
+#define STARTSTOP_H
+
+#include "Motor.hpp" 
+#include "Velocidade.hpp"
+#include "Display.hpp"
+
+class Display;
+class Motor;
+
+
+#define erroAceitavel 0.1
+#define aceleraIdeal 0.5
+#define posServoInicial 15
+#define velocidadeMinima 5
+#define velocidadeMax 30
+#define tempoImcrementoIdeal 100
+#define velZERO 0
+#define PRESSIONADO 1
+#define NOT_PRESSIONADO 0   
+#define switchSS 20
+#define pinFreio 21
+#define tempoMaximoVelocidade 10000
+
+class StartStop {
+public:
+    enum StatesStartStop {
+        stateSwitchON,
+        stateSwitchOFF,
+        stateLigaMotor,
+        stateDesligaMotor,
+        stateEstabilizaAcelera,
+        stateEstabilizaVelocidade,
+        stateManipulaBorboleta,
+        stateStart,
+        stateStop,
+        stateFreando,
+        stateNotLigou,
+        stateNotDesligou,
+        stateDesligaStartStop
+    };
+
+    enum StatesManipulaBorboleta {
+        manterAcelera,
+        manterVelocidadeAcima,
+        manterVelocidadeAbaixo
+    };
+
+    static StatesStartStop switchOFF ();
+
+    static StatesStartStop switchON (); 
+
+    static StatesStartStop ligaMotorSS (Motor &motor, Display &display);
+
+    static StatesStartStop desligaMotorSS (Motor &motor, Display &display);
+
+    static StatesStartStop estabilizaAcelera (Motor &motor);
+    
+    static StatesStartStop estabilizaVelocidade (Motor &motor);
+    
+    static StatesStartStop manipulaBorboleta (Motor &motor, float &tempoUltimoImcremento);
+
+    static StatesStartStop start (Motor &motor);
+    
+    static StatesStartStop stop (Motor &motor);
+
+    static StatesStartStop freando ();
+
+    static StatesStartStop notLigou (Display &display);
+    
+    static StatesStartStop notDesligou (Display &display);
+
+    static StatesStartStop desligaStartStop (Motor &motor, Display &display);
+
+private:
+    static StatesManipulaBorboleta borboleta;
+    static int tentativasLigar;
+    static int tentativasDesligar;
+    static bool inicioVel;
+    static float tempoInicioVel;
+
+};
+
+
+#endif
