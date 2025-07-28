085359d10bba09199c3dd01b69b1e6b554272ba3
AndreMartinsR <andr3mr70223@gmail.com>
2025-07-28 20:43:25 -0300

    teste de aceleraçao borb variavel

---
+++ T_BorbPorAcel/Display.cpp
@@ -0,0 +1,111 @@
+#include "Display.hpp"
+
+volatile unsigned long timeOld = 0;
+
+//Display::Display () {
+//  this->lcd.init();
+//  this->lcd.backlight();
+//  this->lcd.setCursor(0,0);
+//  this->lcd.print("Iniciando M10");
+//  delay(500);
+//  
+//}
+
+void Display:: iniciaDisplay () {
+  this->lcd.init();
+  this->lcd.backlight();
+  this->lcd.setCursor(0,0);
+  this->lcd.print("Iniciando M10");
+  delay(500);
+  this->lcd.clear();
+  Wire.setClock(100000);  // Reduz a velocidade do I2C
+}
+
+
+void Display::mostraTensaoEVel(float velocidade, Motor &motor){
+   //if ( (millis() - timeOld) >= timeInterval){
+    this->lcd.setCursor(0,0);
+    this->lcd.print("DDP:    ");
+    this->lcd.setCursor(5,0);
+    this->lcd.print(Velocidade::getAcelera());
+
+    this->lcd.setCursor(0,1);
+    this->lcd.print("Vel:            ");
+    this->lcd.setCursor(6,1);
+    this->lcd.print(velocidade);
+
+    timeOld = millis();
+  //}
+}
+
+void Display::atualizaDisplay( float velocidade, int FSMState, Motor &motor) {
+
+  String FSMState_str;
+if ( (millis() - timeOld) >= timeInterval){
+
+  mostraTensaoEVel(velocidade, motor);
+
+  switch (FSMState)
+  {
+    case StartStop::stateSwitchOFF:
+      FSMState_str = "SS_off";
+      break;
+
+    case StartStop::stateSwitchON:
+      FSMState_str = "SS__on";
+      break;
+      
+    case StartStop::stateDesligaMotor:
+      FSMState_str = "deslgM";
+      break;
+      
+    case StartStop::stateLigaMotor:
+      FSMState_str = "ligaM ";
+      break;
+      
+    case StartStop::stateEstabilizaAcelera:
+      FSMState_str = "estabA";
+      break;
+      
+//    case StartStop:::
+//      FSMState_str = "estabV";
+//      break;
+
+    case StartStop::stateManipulaBorboleta:
+      FSMState_str = "manipB";
+      break;
+
+    case StartStop::stateStart:
+      FSMState_str = "iniciou";
+      break;
+
+    case StartStop::stateStop:
+      FSMState_str = "parou";
+      break;
+      
+    case StartStop::stateFreando:
+      FSMState_str = "freou";
+      break;
+
+    case StartStop::stateNotLigou:
+      FSMState_str = "notLig";
+      break;
+
+    case StartStop::stateNotDesligou:
+      FSMState_str = "notDsg";
+      break;
+      
+    case StartStop::stateDesligaStartStop:
+      FSMState_str = "deslgSS";
+      break;
+      
+    default:
+      break;
+  }
+  this->lcd.setCursor(10, 0);
+  this->lcd.print(FSMState_str);
+
+  //this->lcd.setCursor(15,1);
+  //this->lcd.print(motor.checaEstadoMotor());
+}
+}
