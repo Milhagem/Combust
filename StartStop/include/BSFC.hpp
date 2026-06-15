81223ee14f7dabb734e1f51dc808606c7bd1f0d2
RICARDO FONSECA LEMOS <ricardofonsecaj123@gmail.com>
2026-06-15 17:05:19 -0300

    Refactor BSFC class and add Kalman filter
    Updated BSFC class to include Kalman filter and new methods.

--- StartStop/include/BSFC.hpp
+++ StartStop/include/BSFC.hpp
@@ -1,44 +1,35 @@
-#ifndef BSFC_H
-#define BSFC_H
+#ifndef BSFC_HPP
+#define BSFC_HPP
 
+#include <Arduino.h>
+#include <SimpleKalmanFilter.h> 
 
-#define pinServo        8
-
-//pinos que precisam ser definidos
-#define pinmap          a
-#define pin02           b    
-#define Pintp           c
-#define SensorCKP       A2
+#define pinmap          3
+#define pin02           2  
+#define Pintp           6
+#define SensorCKP       37 
 
 class Bsfc {
-
  public:
+    // Inicializa o filtro com valores padrão que serão sobrescritos depois
+    Bsfc() : filtroKalmanRPM(20.0, 20.0, 0.5) {}
 
+    void init(); 
     float analisaRPM();
-    
     float analisaPosBorbo();
-
     float analisaMap();
-
     float analisaLambda();
-
-     aceleraEficiente(); //calcula a quantidade de combustível a ser injetada com base nos dados coletados e na curva de consumo específico do motor
     
-
-
-   //Futuras telemetria
     float getPosBorbo() { return posborbo;}
-
     float getLambda() {return lambda;}
-
     float getMap() { return map;}
-
     float getRpm() { return rpm;}
 
+    // Função para receber os parâmetros do MQTT sem reiniciar
+    void atualizaKalman(float mea_e, float est_e, float q);
 
-
-
-    // 
+    static volatile unsigned long tempoAnterior;
+    static volatile unsigned long rpm_calculado; 
 
  private:
     float rpm = 0;
@@ -46,7 +37,17 @@ class Bsfc {
     float lambda = 0;
     float map = 0;
     bool primeiraLeituraTp = true;
+    
+    static volatile unsigned long hw_deltaTAnterior;
+    static int hw_contadorDentes;
+    static int hw_dentesAcumulados;
+    static volatile unsigned long hw_tempoUltimaVolta;
+    static volatile unsigned long hw_tempoAcumulado;
 
+    static void IRAM_ATTR lerCKP();
+
+    // Objeto do filtro encapsulado na classe
+    SimpleKalmanFilter filtroKalmanRPM; 
 };
 
 #endif
